c47252691c1b0830a84d77e0b0ce0903e1ba12c9
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-10-02 14:27:33 +0100

    anonymous

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -10,7 +10,7 @@ import * as Yup from 'yup'
 const Signin = () => {
     const [incorrect, setIncorrect] = useState('')
     // let endpoint = 'http://localhost:4678/signin'
-    let url = 'https://anonymous-back.onrender.com/signin'
+    const endpoint = 'https://anonymous-back.onrender.com/signin'
     let navigate = useNavigate()
     let formik = useFormik({
         initialValues: {
