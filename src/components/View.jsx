a53157e6f845d36b371793b2fabb5afc4a8d578b
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-10-03 15:22:27 +0100

    anonymous

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -37,7 +37,7 @@ const [message, setMessage] = useState([])
   return (
     <>
         <div style={content} className=''>
-            <div className='lg:mb-[150px] md:mb-[150%] mb-[200%]  grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 text-white lg:mt-0 md:mt-0  lg:gap-20 gap-2'>
+            <div className='lg:mb-[200px] md:mb-[200%] mb-[200%]  grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 text-white lg:mt-0 md:mt-0  lg:gap-20 gap-2'>
             { !message ? 'No message' :
                 message.map((item,index)=>(
                     <div key={index} className='border bg-blue-950 lg:w-[250px] md:w-[250px] sm:w-[100%]'>
